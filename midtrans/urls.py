f9bd3b0cadbaa35891211cadf9c87131f4daf9c8
Mazed <raiyoz@live.com>
2025-01-11 07:24:28 +0700

    final

--- midtrans/urls.py
+++ midtrans/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='index'),
     path('checkout/<str:order_id>/', checkout, name='checkout'),
-    path('payment-success/order_id=<str:order_id>&status_code=200&transaction_status=settlement', payment_success, name='payment_success'),
+    path('payment-success/?order_id=<str:order_id>&status_code=200&transaction_status=settlement', payment_success, name='payment_success'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
